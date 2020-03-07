d422122f2fc417f7ecf9b494e1e52097e7953aa1
lijinlei <lijinlei@playcrab.com>
2020-03-07 22:32:07 +0800

    [update]

--- resources/lang/zh/order.php
+++ resources/lang/zh/order.php
@@ -4,8 +4,8 @@ return [
 
     'label'=> '订货',
     'orderDate' => '订单日期',
-    'old_total' => '原价',
-    'now_total' => '现价',
+    'old_total' => '建议零售价',
+    'now_total' => '实付金额',
     'price' => '单价',
 
 ];
