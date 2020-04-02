b2a1cbed9bc0dcb43d056652e72f63b5807c0827
lijinlei <lijinlei@playcrab.com>
2020-04-02 18:30:46 +0800

    [update] action

--- app/Nova/Actions/PaymentStatus.php
+++ app/Nova/Actions/PaymentStatus.php
@@ -38,7 +38,7 @@ class PaymentStatus extends Action
     public function fields()
     {
         return [
-            Select::make(__('order.status'),'stauts')
+            Select::make(__('order.status'),'status')
                 ->options([
                     '0' => '未付款',
                     '1' => '付款',
