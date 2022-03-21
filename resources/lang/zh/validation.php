3e8c72631116192c5dc711d8d380d94aaba9b9bc
lijinlei <133943399@qq.com>
2022-03-21 14:43:21 +0800

    [update] 翻译

--- resources/lang/zh/validation.php
+++ resources/lang/zh/validation.php
@@ -96,7 +96,7 @@ return [
     'password' => 'The password is incorrect.',
     'present' => 'The :attribute field must be present.',
     'regex' => 'The :attribute format is invalid.',
-    'required' => 'The :attribute field 必填.',
+    'required' => ':attribute 必填.',
     'required_if' => 'The :attribute field is required when :other is :value.',
     'required_unless' => 'The :attribute field is required unless :other is in :values.',
     'required_with' => 'The :attribute field is required when :values is present.',
@@ -146,6 +146,9 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+        'orderDate' => '订单日期',
+        'quantity' => '数量',
+    ],
 
 ];
