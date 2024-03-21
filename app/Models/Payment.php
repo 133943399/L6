1fdea3fc80995ffecb74ab7599550f1cadf1d29d
李金垒 <15600006529@qq.com>
2024-03-21 14:22:16 +0800

    [update] 软删除

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Payment extends Model
 {
+    use SoftDeletes;
     /**
      * The attributes that should be cast to native types.
      *
